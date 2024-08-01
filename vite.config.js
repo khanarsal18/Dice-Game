92e509b5014e058eaa29b5c9abfba522a1d3f132
Arsal Khan <editingaddicted7@gmail.com>
2024-08-01 19:07:24 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Dice-Game/',
+  base: '/Dice-Game-React/',
   plugins: [react()],
 });
