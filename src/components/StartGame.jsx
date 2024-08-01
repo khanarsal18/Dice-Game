e4fd29cff133018e57b66161eeb73c545dbcf7a5
Arsal Khan <editingaddicted7@gmail.com>
2024-08-01 16:05:00 +0530

    imgs

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -5,7 +5,7 @@ const StartGame = ({ toggle }) => {
   return (
     <Container>
       <div>
-        <img src="dist/images/dices.png" />
+        <img src="/images/dices.png" alt="nothing"/>
       </div>
       <div className="content">
         <h1>Dice Game</h1>
